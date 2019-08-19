0150c720df20a4b5603b6f07b0e33da75f8791ef
ranitachi <fachran.nazarullah@gmail.com>
2019-08-19 11:17:40 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
-        }
+        // if(env('APP_ENV') !== 'local')
+        // {
+        //     $url->forceSchema('https');
+        // }
     }
 
     /**
