d8351f1aef153908a8ea77a649992229457028c8
ranitachi <fachran.nazarullah@gmail.com>
2020-06-29 11:14:29 +0700

    update

--- resources/views/pages/staf/jadwal/berkas/lembar-penetapan-judul.blade.php
+++ resources/views/pages/staf/jadwal/berkas/lembar-penetapan-judul.blade.php
@@ -106,7 +106,11 @@
                         
                         <td align="left">
                             Depok, {{tgl_indo(date('Y-m-d'))}}<br>
-                            Ketua Sidang Ujian,<br><br><br><br><br><u>{{$pembimbing[0]->dosen->nama}}</u><br>NIP. {{$pembimbing[0]->dosen->nip}}</td>
+                            Ketua Sidang Ujian,<br><br><br><br><br><u>
+                                @if (isset($pembimbing[0]->dosen))
+                                    {{$pembimbing[0]->dosen->nama}}</u><br>NIP. {{$pembimbing[0]->dosen->nip}}
+                                @endif
+                            </td>
                     </tr>
                 </table>	
                 <div>&nbsp;</div>
